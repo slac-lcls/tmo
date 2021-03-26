f0bb45e24ceedb14b6f6ed6dc78362c45033a379
xiangli <xiangli@slac.stanford.edu>
2021-03-26 15:58:32 -0700

    update TMO.py

--- mon/ami/lib/TMO.py
+++ mon/ami/lib/TMO.py
@@ -7,147 +7,47 @@ from ami.flowchart.library.common import CtrlNode
 from ami.flowchart.library.DisplayWidgets import Histogram2DWidget
 from pyqtgraph import QtCore
 from psana.pop.POP import POP as psanaPOP
+from psalg_ext import peak_finder_algos
+from psana.hexanode.HitFinder import HitFinder
+from scipy.signal import find_peaks
+from scipy.ndimage import gaussian_filter1d
 
 
-class Histogram2DRoi(CtrlNode):
 
-    """
-    Roi of 2d histogram
-    """
-
-    nodeName = "Histogram2DRoi"
-    uiTemplate = [('origin x', 'intSpin', {'value': 0, 'min': 0}),
-                  ('origin y', 'intSpin', {'value': 0, 'min': 0}),
-                  ('extent x', 'intSpin', {'value': 10, 'min': 1}),
-                  ('extent y', 'intSpin', {'value': 10, 'min': 1})]
-
-    def __init__(self, name):
-        super().__init__(name, terminals={'XBins': {'io': 'in', 'ttype': Array1d},
-                                          'YBins': {'io': 'in', 'ttype': Array1d},
-                                          'Counts': {'io': 'in', 'ttype': Array2d},
-                                          'XBins.Out': {'io': 'out', 'ttype': Array1d},
-                                          'YBins.Out': {'io': 'out', 'ttype': Array1d},
-                                          'Counts.Out': {'io': 'out', 'ttype': Array2d}},
-                         viewable=True)
-
-    def display(self, topics, terms, addr, win, **kwargs):
-        super().display(topics, terms, addr, win, Histogram2DWidget, **kwargs)
-
-        if self.widget:
-            self.roi = pg.RectROI([self.values['origin x'], self.values['origin y']],
-                                  [self.values['extent x'], self.values['extent y']])
-            self.roi.sigRegionChangeFinished.connect(self.set_values)
-            self.widget.view.addItem(self.roi)
-
-        return self.widget
-
-    def set_values(self, *args, **kwargs):
-        # need to block signals to the stateGroup otherwise stateGroup.sigChanged
-        # will be emmitted by setValue causing update to be called
-        self.stateGroup.blockSignals(True)
-        roi = args[0]
-        extent, _, origin = roi.getAffineSliceParams(self.widget.imageItem.image, self.widget.imageItem)
-        origin = QtCore.QPoint(*origin)*self.widget.transform
-        extent = QtCore.QPoint(*extent)
-        self.values['origin x'] = origin.x()
-        self.values['origin y'] = origin.y()
-        self.values['extent x'] = extent.x()
-        self.values['extent y'] = extent.y()
-        self.ctrls['origin x'].setValue(self.values['origin x'])
-        self.ctrls['extent x'].setValue(self.values['extent x'])
-        self.ctrls['origin y'].setValue(self.values['origin y'])
-        self.ctrls['extent y'].setValue(self.values['extent y'])
-        self.stateGroup.blockSignals(False)
-        self.sigStateChanged.emit(self)
-
-    def update(self, *args, **kwargs):
-        super().update(*args, **kwargs)
-
-        if self.widget:
-            origin = QtCore.QPoint(self.values['origin x'], self.values['origin y'])
-            self.roi.setPos(origin.x(), origin.y(), finish=False)
-            self.roi.setSize((self.values['extent x'], self.values['extent y']), finish=False)
-
-    def to_operation(self, inputs, conditions={}):
-        outputs = self.output_vars()
-
-        ox = self.values['origin x']
-        ex = self.values['extent x']
-        oy = self.values['origin y']
-        ey = self.values['extent y']
-
-        def func(x, y, img):
-            xstart = np.digitize(ox, x)
-            ystart = np.digitize(oy, y)
-            xs = slice(xstart, xstart+ex)
-            ys = slice(ystart, ystart+ey)
-            return x[xs], y[ys], img[xs, ys]
-
-        node = gn.Map(name=self.name()+"_operation",
-                      condition_needs=conditions, inputs=inputs, outputs=outputs,
-                      func=func,
-                      parent=self.name())
-        return node
 
 
 class POPProc(object):
 
     def __init__(self, args):
         self.args = args
-        # self.calibconsts = {}
         self.accum_num = args.pop('accum_num', 1)
         self.normalize_dist = args.pop('normalizeDist', True)
+        self.s = [1 if args.pop('Q'+str(i+1), True) else 0 for i in range(4)]
         self.proc = None
-        self.img = None
         self.counter = 0
 
     def __call__(self, img):
-    #     init = False
-    #     if self.calibconsts.keys() != calib.keys():
-    #         init = True
-    #     elif not all(np.array_equal(self.calibconsts[key], calib[key]) for key in calib):
-    #         init = True
 
         if self.proc is None:
             self.img = np.zeros(img.shape)
-            self.counter += 1
             self.proc = psanaPOP(img=img, **self.args)
 
-            self.slice_img = np.array([[np.nan]])
-            self.rbins = np.array([np.nan, np.nan])
-            self.distr = np.array([np.nan])
-            self.ebins = np.array([np.nan, np.nan])
-            self.diste = np.array([np.nan])
-
-        if self.accum_num == 1:
-            pop = self.proc
-            pop.Peel(img)
-            slice_img = pop.GetSlice()
-            rbins, distr = pop.GetRadialDist()
-            ebins, diste = pop.GetEnergyDist()
-            if self.normalize_dist:
-                distr = distr/distr.max()
-                diste = diste/diste.max()
-
-            return slice_img, rbins, distr, ebins, diste
-
         self.img += img
+        self.counter += 1
 
         if self.counter % self.accum_num == 0:
-            pop = self.proc
-            pop.Peel(self.img)
-            self.slice_img = pop.GetSlice()
-            self.rbins, self.distr = pop.GetRadialDist()
-            self.ebins, self.diste = pop.GetEnergyDist()
+            self.proc.Peel(self.img,s = self.s)
+            self.slice_img = self.proc.GetSlice()
+            self.rbins, self.distr = self.proc.GetRadialDist()
+            self.ebins, self.diste = self.proc.GetEnergyDist()
+           # self.betas = self.GetBetas()
             self.counter = 0
-            self.img = np.zeros(img.shape)
+
             if self.normalize_dist:
                 self.distr = self.distr/self.distr.max()
                 self.diste = self.diste/self.diste.max()
-        else:
-            self.counter += 1
 
-        return self.slice_img, self.rbins, self.distr, self.ebins, self.diste
+        return self.slice_img, self.rbins[:-1], self.distr,self.ebins[:-1], self.diste
 
 
 class POP(CtrlNode):
@@ -157,23 +57,28 @@ class POP(CtrlNode):
     """
 
     nodeName = "POP"
+      
 
     uiTemplate = [('RBFs_fnm', 'text'),
                   ('lmax', 'intSpin', {'value': 4, 'values': ['2', '4', '6', '8', '10', '12']}),
                   ('reg', 'doubleSpin', {'value': 0}),
-                  ('alpha', 'doubleSpin', {'value': 4e-4}),
-                  ('X0', 'intSpin', {'value': 512}),
-                  ('Y0', 'intSpin', {'value': 512}),
-                  ('Rmax', 'intSpin', {'value': 512}),
+                  ('alpha', 'doubleSpin', {'value': 0.0013}),
+                  ('X0', 'intSpin', {'value': 274}),
+                  ('Y0', 'intSpin', {'value': 276}),
+                  ('Rmax', 'intSpin', {'value': 235}),
                   ('edge_w', 'intSpin', {'value': 10}),
-                  ('accum_num', 'intSpin', {'value': 30, 'min': 0}),
-                  ('normalizeDist', 'check', {'checked': True})]
+                  ('accum_num', 'intSpin', {'value': 1, 'min': 0}),
+                  ('normalizeDist', 'check', {'checked': True}),
+                  ('Q1', 'check', {'checked': True}),
+                  ('Q2', 'check', {'checked': True}),
+                  ('Q3', 'check', {'checked': True}),
+                  ('Q4', 'check', {'checked': True})]
 
     def __init__(self, name):
         super().__init__(name, terminals={'Image': {'io': 'in', 'ttype': Array2d},
                                           'sliceImg': {'io': 'out', 'ttype': Array2d},
                                           'Rbins': {'io': 'out', 'ttype': Array1d},
-                                          'DistR': {'io': 'out', 'ttype': Array1d},
+                                          'DistR': {'io': 'out', 'ttype': Array1d},                                        
                                           'Ebins': {'io': 'out', 'ttype': Array1d},
                                           'DistE': {'io': 'out', 'ttype': Array1d}})
 
@@ -185,3 +90,93 @@ class POP(CtrlNode):
                       inputs=inputs, outputs=outputs, parent=self.name(),
                       func=POPProc(self.values))
         return node
+        
+
+class QUADProc(object):
+
+    def __init__(self, args):
+    
+        self.args = args
+        self.HF = HitFinder(self.args)
+        self.channels = {'mcp':0,'x1':1,'x2':2,'y1':3,'y2':4}
+
+    def __call__(self, times, wfs):  
+        
+        ts = times[self.args['ind1']:self.args['ind2']]
+        heights = {}
+       
+        for k in self.channels.keys():
+            if k =='mcp':
+                heights[k] = np.max(wfs[self.channels[k]][-1000:])+self.args[k+'_thresh']
+            else:
+                heights[k] = np.min(-wfs[self.channels[k]][-1000:])+self.args[k+'_thresh']           
+
+    
+    
+        t_pks = {}
+        for i,k in enumerate(self.channels.keys()):
+
+            if k=='mcp':
+                ind_t_pks,_ = find_peaks(gaussian_filter1d(wfs[self.channels[k]][self.args['ind1']:self.args['ind2']],10),
+                               height=heights[k],prominence=self.args[k+'_prom'])
+            else:
+                ind_t_pks,_ = find_peaks(gaussian_filter1d(-wfs[self.channels[k]][self.args['ind1']:self.args['ind2']],10),
+                                     height=heights[k],prominence=self.args[k+'_prom'])                        
+            t_pks[k] = ts[ind_t_pks[:min(len(ind_t_pks),self.args['max_pknum'])]]*1e9    
+
+        self.HF.FindHits(t_pks['mcp'],t_pks['x1'],t_pks['x2'],t_pks['y1'],t_pks['y2'])
+        xs1,ys1,ts1 = self.HF.GetXYT()        
+
+        return xs1,ys1,ts1   
+
+
+class QUAD(CtrlNode):
+
+    """
+    peak_finder+psana_HitFinder
+    """
+
+    nodeName = "QUAD"
+
+
+    uiTemplate = [('ind1', 'intSpin', {'value': 0}),
+                  ('ind2', 'intSpin', {'value': 60000}), 
+                  ('max_pknum', 'intSpin', {'value': 30}), 
+                  ('mcp_thresh', 'doubleSpin', {'value': 2}),
+                  ('x1_thresh', 'doubleSpin', {'value': 2}),
+                  ('x2_thresh', 'doubleSpin', {'value': 2}),  
+                  ('y1_thresh', 'doubleSpin', {'value': 2}),
+                  ('y2_thresh', 'doubleSpin', {'value': 2}), 
+                  ('mcp_prom', 'doubleSpin', {'value': 5}),                   
+                  ('x1_prom', 'doubleSpin', {'value': 20}),
+                  ('x2_prom', 'doubleSpin', {'value': 20}),  
+                  ('y1_prom', 'doubleSpin', {'value': 20}),
+                  ('y2_prom', 'doubleSpin', {'value': 20}),                                                       
+                  ('runtime_u', 'doubleSpin', {'value': 185}),
+                  ('runtime_v', 'doubleSpin', {'value': 185}),
+                  ('tsum_avg_u','doubleSpin', {'value': 189}),
+                  ('tsum_avg_v', 'doubleSpin', {'value': 195.5}),    
+                  ('tsum_hw_u', 'doubleSpin', {'value': 10}),
+                  ('tsum_hw_v','doubleSpin', {'value': 10}),                                
+                  ('f_u', 'doubleSpin', {'value': 0.65}),
+                  ('f_v', 'doubleSpin', {'value': 0.65}),
+                  ('Rmax', 'doubleSpin', {'value': 65})]
+                  
+                  # entry point must be called func
+    
+
+    def __init__(self, name):
+        super().__init__(name, terminals={'Times': {'io': 'in', 'ttype': Array1d},
+                                          'WFs': {'io': 'in', 'ttype': Array2d},
+                                          'X': {'io': 'out', 'ttype': Array1d},
+                                          'Y': {'io': 'out', 'ttype': Array1d},
+                                          'T': {'io': 'out', 'ttype': Array1d}})
+
+    def to_operation(self, inputs, conditions={}):
+        outputs = self.output_vars()
+
+        node = gn.Map(name=self.name()+"_operation",
+                      condition_needs=conditions,
+                      inputs=inputs, outputs=outputs, parent=self.name(),
+                      func=QUADProc(self.values))
+        return node                
