57caf5ec43edcae273297141882334c7ec6e13f9
xiangli <xiangli@slac.stanford.edu>
2021-05-20 18:13:49 -0700

    update lx08 PreProc script

--- ana/instrument/MBES/lx08/PreProc.py
+++ ana/instrument/MBES/lx08/PreProc.py
@@ -17,7 +17,8 @@ os.environ['PS_SRV_NODES']='1'
 
 exp = 'tmolx0819'
 run_num = '11'
-params = {'exp':exp,'run_num':int(run_num),'refresh_num':100,'hdf5':'/reg/d/psdm/tmo/tmolx0819/scratch/xiangli/v_run_tst_'}
+h5_path = '/reg/d/psdm/tmo/tmolx0819/scratch/xiangli/'
+params = {'exp':exp,'run_num':int(run_num),'refresh_num':100,'hdf5':h5_path+'run_'}
 #print(params)
 params = params 
 params['hdf5'] = params['hdf5']+str(params['run_num'])+'.h5'
@@ -38,30 +39,80 @@ for myrun in ds.runs():
     hsd = myrun.Detector('hsd')   
 
     det_evt = myrun.Detector('timing')
+    det_gmd = myrun.Detector('gmd')
+    det_xgmd = myrun.Detector('xgmd')
+    det_ebm = myrun.Detector('ebeam')
+
+    det_las_t = myrun.Detector('las_fs14_target_time')
+    det_las_d = myrun.Detector('las_fs14_target_time_dial')
 
     evt_dict = {}
     sevt_dict = {}
+    maxNumPks = 100
 
 
     sinit=None
+    ts = None
+    ts_p = None
 
     for nevt,evt in enumerate(myrun.events()):
+        
+        tpks = np.array([np.nan]*maxNumPks)
+        tpks_p = np.array([np.nan]*maxNumPks)        
 
+        hpks = np.array([np.nan]*maxNumPks)
+        hpks_p = np.array([np.nan]*maxNumPks)
 
+        # Machine data
         evt_dict['evt'] = np.array(det_evt.raw.eventcodes(evt),dtype=np.int)
+        evt_dict['gmd'] = det_gmd.raw.energy(evt)
+        evt_dict['xgmd'] = det_xgmd.raw.energy(evt)
+        evt_dict['pho'] = det_ebm.raw.ebeamPhotonEnergy(evt)
+        evt_dict['ebm_l3'] = det_ebm.raw.ebeamL3Energy(evt)
+        evt_dict['las_t'] = det_las_t(evt)
+        evt_dict['las_d'] = det_las_d(evt)
+        if evt_dict['las_d'] == None or evt_dict['gmd'] == None or evt_dict['xgmd'] == None:
+            continue
+        evt_dict['gmd'] = evt_dict['gmd']*1000
+        evt_dict['xgmd'] = evt_dict['xgmd']*1000            
+
+        if evt_dict['las_t'] == None:
+            evt_dict['las_t'] = np.nan   
             
         ##Ion data
         wfs = hsd.raw.waveforms(evt)
         if wfs is None:
             continue
         wf_mcp = wfs[0][0].astype(np.float)
+        
+        if ts is None:
+            ts = np.arange(0,len(wf_mcp))*0.167
         wf_mcp = wf_mcp - np.mean(wf_mcp[-100:])
 
+        pks,prop = find_peaks(wf_mcp, prominence = 20)
+        
+        npks = min(len(pks),maxNumPks)
+        tpks[:npks] = ts[pks][:npks]
+        hpks[:npks] = prop[:npks]
 
-        wf0 = np.zeros_like(wf_mcp)
-        pks,prop = find_peaks(wf_mcp, prominence = 10)
-        wf0[pks] = wf0[pks] + prop['prominences']
+        evt_dict['n_pks'] = npks
+        evt_dict['t_pks'] = tpks
+        evt_dict['h_pks'] = hpks
+        
 
+        
+###############################
+        wfs = hsd.raw.padded(evt)
+        if wfs is None:
+            continue
+        wf_mcp = wfs[0][0].astype(np.float)
+        
+        if ts_p is None:
+            ts_p = np.arange(0,len(wf_mcp))*0.167
+            
+        wf_mcp = wf_mcp - np.mean(wf_mcp[-100:])
+        
+        
         if sinit==None:
 
             sinit = 1
@@ -71,11 +122,24 @@ for myrun in ds.runs():
             sevt_dict['num_evt'] = 0
 
 
+        wf0 = np.zeros_like(wf_mcp)
+        pks,prop = find_peaks(wf_mcp, prominence = 20)
+        wf0[pks] = wf0[pks] + prop['prominences']
+        
+        npks_p = min(len(pks),maxNumPks)
+        tpks_p[:npks] = ts[pks][:npks]
+        hpks_p[:npks] = prop[:npks]
+        
+        evt_dict['n_pks_padded'] = npks_p
+        evt_dict['t_pks_padded'] = tpks_p
+        evt_dict['h_pks_padded'] = hpks_p        
+
+
         sevt_dict['wf'] += wf_mcp
         sevt_dict['wf_pks'] += wf0
-        sevt_dict['num_evt'] += 0
+        sevt_dict['num_evt'] += 1        
 
-
+###############################
         smd.event(evt, evt_dict)                
 
 
